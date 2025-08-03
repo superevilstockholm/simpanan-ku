a55ed0f17efab5242023a47515bca6d90e41dedc
superevilstockholm <balilombok11@gmail.com>
2025-08-03 12:17:46 +0700

    Created migrations for data classes, student, and teacher

---
+++ database/migrations/2025_08_03_050501_create_data_student_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data_student', function (Blueprint $table) {
+            $table->id();
+            $table->string('nisn', 10)->unique();
+            $table->string('fullname', 255);
+            $table->enum('gender', ['L', 'P']);
+            $table->date('dob');
+            $table->integer('class_id')->constrained('data_classes');
+            $table->integer('user_id')->constrained('users')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data_student');
+    }
+};
