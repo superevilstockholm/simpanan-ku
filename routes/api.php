a7c69accca62a4dd96218261e317a417007b8afc
superevilstockholm <balilombok11@gmail.com>
2025-08-04 22:18:32 +0700

    Created Role Middleware and updated user model to using hasapitoken from sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Controllers
+use App\Http\Controllers\Auth\AuthController;
+
+// Master Data
+use App\Http\Controllers\MasterData\UserController;
+use App\Http\Controllers\MasterData\DataClassesController;
+
+// Auth
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('role:admin')->group(function () {
+        // Master Data
+        Route::apiResource('data-users', UserController::class)->parameters([
+            'data-users' => 'user'
+        ]);
+        Route::apiResource('data-classes', DataClassesController::class)->parameters([
+            'data-classes' => 'dataClasses'
+        ]);
+    });
+});
